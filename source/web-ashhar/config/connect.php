e128ef268fdb5057a18388faa28676a4b848fb1e
Ashhar Hasan <hashhar_dev@outlook.com>
2015-10-16 22:31:41 +0530

    Made login.php, session.php, connect.php
    Login is working as a proof of concept

---
+++ source/web-ashhar/config/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+error_reporting(0);
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mydb');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mysql/@@$hu0711');
+
+// Connect and Select Database
+mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD) or die('Database Error: Connection Failed'.mysql_error());
+mysql_select_db(DB_NAME) or die('Database Error: DB to Load'.mysql_error());
+
+?>
---
+++ source/web-ashhar/include/login.php
@@ -0,0 +1,36 @@
+<?php
+require '/config/connect.php';
+require '/config/session.php';
+
+if(isset($_POST['user'],  $_POST['pass']))
+{
+  $uName = $_POST['user']; $pass = $_POST['pass'];
+  // Check whether the user already exists
+  // Fixme: SQL Injection Here!
+  $result = mysql_query("SELECT PassHash, UserName FROM CUSTOMER WHERE UserName = '$uName'");
+  if (mysql_num_rows($result) > 0)
+  {
+    $row = mysql_fetch_row($result);
+    // if (md5($pass) == $row[0])
+    if ($pass == $row[0])
+    {
+      echo("Successfully logged in");
+      $_SESSION['loggedin'] = 1;
+      $_SESSION['user'] = $uName;
+
+      $result = mysql_query("SELECT FName_CUST FROM CUSTOMER WHERE UserName = '$row[1]'");
+      $row = mysql_fetch_row($result);
+
+      $_SESSION['name'] = $row[0];
+    }
+    else
+      echo "Either Username or Password is Incorrect";
+  }
+  else
+    echo "User Does Not Exists";
+}
+else
+{
+  echo("Bitch Please!");
+}
+?>
