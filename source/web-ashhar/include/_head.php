7226c343f7d85102741021a4edc34b661a8e8cfb
Ashhar Hasan <hashhar_dev@outlook.com>
2015-10-19 19:00:38 +0530

    Added helper include file (currently empty)

---
+++ source/web-ashhar/include/_head.php
@@ -0,0 +1,5 @@
+<?php
+echo '<link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" />';
+echo '<link href="assets/css/carousel.css" rel="stylesheet" />';
+echo '<link href="assets/css/login.css" rel="stylesheet" />';
+?>
